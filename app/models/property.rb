a7169e5e3a8d70ce10ad388339350a7103e6afa7
Yumiko <wakinghira73@gmail.com>
2021-05-13 16:28:10 +0900

    add:バリデーション

--- app/models/property.rb
+++ app/models/property.rb
@@ -2,4 +2,10 @@ class Property < ApplicationRecord
   has_many :nearest_station, dependent: :destroy
   #refect_all_blank関数でtrueが出たパラメータは送信データから除外ざせる
   accepts_nested_attributes_for :nearest_station, allow_destroy: true, reject_if: :all_blank
+
+  validates :property_name, presence: true
+  validates :rent, presence: true
+  validates :address, presence: true
+  validates :age, presence: true
+  validates :remarks, presence: true
 end
