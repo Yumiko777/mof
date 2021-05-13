8ebb8e545afa6b9b053fd4dbab39355eb0d4db2f
Yumiko <wakinghira73@gmail.com>
2021-05-13 16:09:47 +0900

    add:最寄駅追加

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,3 +1,5 @@
 class Property < ApplicationRecord
-  has_many :nearest_station
+  has_many :nearest_station, dependent: :destroy
+  #refect_all_blank関数でtrueが出たパラメータは送信データから除外ざせる
+  accepts_nested_attributes_for :nearest_station, allow_destroy: true, reject_if: :all_blank
 end
